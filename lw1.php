354bf41e4970956e39576ac6de3dcff7a931d52a
ydekor <114324139+ydekor@users.noreply.github.com>
2022-12-16 21:10:15 +0300

    second

---
+++ lw1.php
@@ -0,0 +1,42 @@
+<?php
+
+class Calculator
+{
+    private float $result = 0.0;
+    public function sum(float $sum): self
+    {
+        $this->result += $sum;
+        return $this;
+    }
+    public function minus(float $minus): self
+    {
+        $this->result -= $minus;
+        return $this;
+    }
+    public function product(float $product): self
+    {
+        $this->result *= $product;
+        return $this;
+    }
+    public function division(float $division): self
+    {
+        if ($division !== 0.0) {
+            $this->result /= $division;
+        } else {
+            $this->result = 0.0;
+        }
+        return $this;
+    }
+    public function getResult(): string
+    {
+        return $this->result;
+    }
+}
+
+$calculator = new Calculator();
+
+echo $calculator->sum(1)->sum(2)->product(3)->division(3)->getResult();
+
+echo $calculator->sum(3)->sum(3)->minus(3)->division(3)->getResult();
+// echo $calculator->sum(1.6)->sum(2.4)->product(4)->getResult();
+// echo $calculator->sum(1)->sum(2)->product(3)->division(0)->getResult();
